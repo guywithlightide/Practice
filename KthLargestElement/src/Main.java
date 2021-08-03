8ea013032e3ea74252471b03441783f088048c1a
guywithlightide <ksohams97@gmail.com>
2021-08-03 21:18:58 +0530

    first-commit

---
+++ KthLargestElement/src/Main.java
@@ -0,0 +1,39 @@
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		int [] arr = {45,6,78,2,32};
+		int k=1;
+		Map<Integer, Integer> numVFreq = new TreeMap<>();
+		for(int i=0;i<arr.length;i++)
+		{
+			Integer z = numVFreq.get(arr[i]);
+			if(z==null)
+				numVFreq.put(arr[i], 1);
+			else
+				numVFreq.put(arr[i], z++);
+		}
+		int total = 0;
+		int number = arr[0];
+		boolean largest = true;
+		for(int num : numVFreq.keySet())
+		{
+			total+=numVFreq.get(num);
+			number = num;
+			if(!largest)
+			{
+				if(total>=k)
+					break;				
+			}
+			else
+			{
+				if(total >= arr.length-k+1)
+					break;
+			}
+		}
+		System.out.println(k+(largest?" largest":" smallest")+" element is "+number);
+	}
+
+}
